fecf1d1f1dd4e14a5e1d07d7bd08b95dae05159c
Quicksilvr23 <yhs@12451-student.yhs.local>
2015-09-14 14:55:18 -0700

    Version 1.0

---
+++ PreLab.java
@@ -0,0 +1,14 @@
+//****************************************************
+//File Name: PreLab.java
+//
+//purpose: To print something 
+//****************************************************
+
+public class PreLab
+{
+  public static void main(String[] args)
+  {
+    System.out.println ("Computer Science, YES!");
+    System.out.print("======================");
+  }
+}
