97e80b511c20735d0681975bf0e2ece7ba04ba83
Quicksilvr23 <yhs@12451-student.yhs.local>
2015-09-14 15:14:09 -0700

    Count version 1.0

---
+++ Count.java
@@ -0,0 +1,18 @@
+//****************************************************
+//File Name: Poem.java     
+//Creater: JessicaW17
+//purpose: To print a poem 
+//****************************************************
+
+public class Count
+{
+    public static void main (String[] args)
+    {
+      //This line counts to five in english
+      System.out.println ("one two three four five");
+      //This line counts to five in french
+      System.out.println ("un deux trois quatre cinq");
+      //This line counts to five in spanish
+      System.out.println ("uno dos tres cuatro cinco");
+    }
+}
