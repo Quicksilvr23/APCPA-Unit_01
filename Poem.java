17df0e95ed8a891515013e93ae0dacf3a76e3046
Quicksilvr23 <yhs@12451-student.yhs.local>
2015-09-14 15:03:29 -0700

    Poem Version 1.0

---
+++ Poem.java
@@ -0,0 +1,16 @@
+//****************************************************
+//File Name: Poem.java
+//
+//purpose: To print a poem 
+//****************************************************
+
+public class Poem
+{
+  public static void main(String[] args)
+  {
+    System.out.println ("Roses are Red");
+    System.out.println("Violets are Blue");
+    System.out.println("Sugar is Sweet");
+    System.out.print("And so are YOU!");
+  }
+}
